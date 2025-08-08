036dd796e3b5f8e451640e0b6e274a5e16065979
jorgegomezgerling <jorgegomezgerling@gmail.com>
2025-08-08 09:53:14 -0300

    incremental, bosquejo

--- extraccion_incremental_api.py
+++ extraccion_incremental_api.py
@@ -6,13 +6,14 @@ load_dotenv()
 
 api_key = os.getenv("API_KEY")
 base_url = os.getenv("BASE_URL")
-full_endpoint = os.getenv("FULL_ENDPOINT")
+incremental_endpoint = os.getenv("INCREMENTAL_ENDPOINT")
 
 headers = {
     'Authorization': f'Bearer {api_key}'
 }
 
-tickers = get_data(base_url,full_endpoint, data_field='results',headers=headers)
-tickers_df = build_table(tickers)
+short_volume = get_data(base_url,incremental_endpoint,data_field='results',headers=headers)
+print(short_volume)
+# short_volume_df = build_table(short_volume)
 # save_data_as_delta(tickers_df, "data_lake/tickers", mode='overwrite')
-print(tickers_df)
+# print(short_volume_df.tail())
