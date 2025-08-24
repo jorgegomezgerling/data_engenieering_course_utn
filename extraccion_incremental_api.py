9c5af3058ce69f37125ad32d6e7beab9cb8976dd
jorgegomezgerling <jorgegomezgerling@gmail.com>
2025-08-24 15:20:06 -0300

    cambios en gitignore

--- extraccion_incremental_api.py
+++ extraccion_incremental_api.py
@@ -25,5 +25,5 @@ params = {
 short_volume = get_data_paginacion(base_url,incremental_endpoint,data_field='results',params=params,headers=headers)
 short_volume_df = build_table(short_volume)
 print(short_volume_df)
-save_new_data_as_delta(short_volume_df, 'data_lake/short_volume', 'total_volume')
+save_new_data_as_delta(short_volume_df, 'data_lake/bronze/short_volume', 'total_volume')
 
